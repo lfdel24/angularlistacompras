560495efe5dc7a85ec8bdcd72822318ac3f6b3e4
lfdel24 <lfdel24@gmail.com>
2021-07-10 21:18:45 -0500

    Update header component

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Item } from 'src/app/models/item';
 import { ItemsService } from 'src/app/services/items.service';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-create',
@@ -9,7 +11,10 @@ import { ItemsService } from 'src/app/services/items.service';
 })
 export class CreateComponent implements OnInit {
 
-  constructor(private service: ItemsService) {
+  constructor(
+    private service: ItemsService,
+    private route: Router
+  ) {
 
     this.inputTitle.valueChanges.subscribe(
       value => console.log(value));
@@ -25,14 +30,29 @@ export class CreateComponent implements OnInit {
   inputCompleted: FormControl = new FormControl('');
 
   create(): void {
+    this.service.add(this.getItem());
+    this.resetInputs();
+    console.log(this.service.items);
+    //TODO: create class route url
+    this.route.navigateByUrl('');
+  }
+
+  getItem() {
     let item: Item = new Item();
+    //TODO: Debe ser dynamic this.service.items.length + 1
     item.id = this.service.items.length + 1;
     item.title = this.inputTitle.value;
     item.price = this.inputPrice.value;
     item.quantity = this.inputQuantity.value;
     item.completed = this.inputCompleted.value;
-    this.service.add(item);
-    console.log(this.service.items);
+    return item;
+  }
+
+  resetInputs() {
+    this.inputTitle.setValue('');
+    this.inputPrice.setValue('');
+    this.inputQuantity.setValue('');
+    this.inputCompleted.setValue('');
   }
 
 }
